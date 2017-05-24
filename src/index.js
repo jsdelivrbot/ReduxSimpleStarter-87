cff8ee30081d5cf1c41b89da98f009a8b82bc65c
MiguelRamBalt <miguel.ramirez.baltazar@gmail.com>
2017-05-23 22:42:35 -0500

    lodash adding on Search Bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -31,8 +32,10 @@ class App extends Component {
   }
 
   render(){
+    const videoSearched = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return( <div>
-      <SearchBar onSearchBarChange={term=>this.videoSearch(term)} />
+      <SearchBar onSearchBarChange={videoSearched} />
       <VideoDetail video={this.state.Selectedvideo}/>
       <VideoList
         onVideoSelect ={Selectedvideo => this.setState({Selectedvideo})}
